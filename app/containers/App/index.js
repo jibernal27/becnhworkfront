0a24de8779d797366f592fc366b319ee9e62321a
Jairo Bernal <jairo.bernal@send.sd>
2020-03-09 03:50:01 -0500

    initial commit

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,7 +12,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-
+import SingUp from 'containers/SignUp/Loadable';
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
@@ -20,6 +20,7 @@ export default function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/signup" component={SingUp} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
